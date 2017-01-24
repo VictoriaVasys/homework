3a42e53b66e4939cb59c396d92853ae8ec0b3668
VictoriaVasys <vvasys@gmail.com>
2017-01-24 12:11:16 -0700

    update 3

--- prime_1.rb
+++ prime_1.rb
@@ -1,14 +1,24 @@
 def prime?(number)
+    
     if number.class == "Float"
         puts "#{number} is not prime"
     elsif number == 0 || number == 1 || number == 2
         puts "#{number} is not prime"
     else
         n_new = number / 2
+        number_of_non_divisibles = []
         while n_new > 2 
-            number % n_new - 1 = 0
-            puts "#{number} is not prime"
+            if number % n_new - 1 == 0
+                puts "#{number} is not prime"
+                n_new = 1
+            else 
+                number_of_non_divisibles << n_new
+                # need to continue to check & make sure no % == 0; add all responses to an array?
+            end
             n_new -=1
+        end
+        if number_of_non_divisibles.count == 1
+            puts "#{number} is a prime!"
         end
     end
  
