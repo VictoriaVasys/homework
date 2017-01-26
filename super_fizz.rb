14525b5a5ceb8dadec9e0113c27eca65fcce0c51
VictoriaVasys <vvasys@gmail.com>
2017-01-25 23:50:25 -0700

    initializing pseudocode for  super_fizz

---
+++ super_fizz.rb
@@ -0,0 +1,21 @@
+## psuedocode
+
+# establish array 1..1000
+# for each number in array:
+# if it's divisible by 7
+  # print "Super"
+  # if it's also divisible by 5
+    # print "SuperBuzz"
+    # if it's also divisible by 3
+      # print "SuperFizzBuzz"
+  # if it's also divisible by 3
+    # print "SuperFizz"
+# else if it's divisible by 5
+  # print "Buzz"
+  # if it's also divisible by 3
+    # print "FizzBuzz"
+# else if it's divisible by 3
+  # print "Fizz"
+# else 
+  # print the number
+# exit the loop
