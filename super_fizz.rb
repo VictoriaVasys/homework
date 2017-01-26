6d419a09208c461a9b6bd3a5d8356ad3ff368650
VictoriaVasys <vvasys@gmail.com>
2017-01-26 00:17:55 -0700

    Working super_fizz.rb

--- super_fizz.rb
+++ super_fizz.rb
@@ -1,7 +1,28 @@
-## psuedocode
 
-# establish array 1..1000
-# for each number in array:
+array = Array (0..1000)
+
+array.each do |num|
+  if num % 7 == 0 && num % 5 == 0 && num % 3 == 0
+    puts "SuperFizzBuzz"
+  elsif num % 7 == 0 && num % 5 == 0
+    puts "SuperBuzz"
+  elsif num % 7 == 0 && num % 3 == 0
+    puts "SuperFizz"
+  elsif num % 5 == 0 && num % 3 == 0
+    puts "FizzBuzz"
+  elsif num % 7 == 0
+    puts "Super"
+  elsif num % 5 == 0
+    puts "Buzz"
+  elsif num % 3 == 0
+    puts "Fizz"
+  else
+    puts num
+  end
+end
+
+      
+
 # if it's divisible by 7
   # print "Super"
   # if it's also divisible by 5
